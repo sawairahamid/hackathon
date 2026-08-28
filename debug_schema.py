df0139620d2520e3a40b7514e07c12c8d9c34c1b
Muhammad Taha Nadeem <tahanadeem478@gmail.com>
2026-08-29 00:57:07 +0500

    i

--- debug_schema.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from app.trace import db_path, _connect
-
-conn = _connect()
-cur = conn.cursor()
-cur.execute("PRAGMA table_info(workflows)")
-for row in cur.fetchall():
-    print(dict(row))
